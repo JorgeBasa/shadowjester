a03103e84290afa59a96b593c21038b776624277
JorgeBasa <73369979+JorgeBasa@users.noreply.github.com>
2023-11-21 16:04:06 -0700

    Delete PROYECTO MARKETING directory
    Esto no funciono, lo intentaré de otro modo

--- PROYECTO MARKETING/ESCRITURAS/dialogoschidos.js
+++
@@ -1,9 +0,0 @@
-const modalItself = document.querySelector('#modal3');
-const openModal = document.querySelector('#story3');
-const closeModal = document.querySelector('#EndStory');
-
-
-openModal.addEventListener('click', () => {
-    modalItself.showModal();
-    return modalItself;
-});
