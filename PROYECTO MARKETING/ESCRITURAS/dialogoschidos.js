c6a2bff11834af19afa74c5dd0d19271d03f0daa
JorgeBasa <73369979+JorgeBasa@users.noreply.github.com>
2023-11-21 15:44:20 -0700

    Add files via upload

---
+++ PROYECTO MARKETING/ESCRITURAS/dialogoschidos.js
@@ -0,0 +1,9 @@
+const modalItself = document.querySelector('#modal3');
+const openModal = document.querySelector('#story3');
+const closeModal = document.querySelector('#EndStory');
+
+
+openModal.addEventListener('click', () => {
+    modalItself.showModal();
+    return modalItself;
+});
